2a9d98dd2f5617b0f3c70294784f4f9bb32df5f9
Rok Spacapan <rok.spacapan@sledat.com>
2019-10-14 13:54:15 +0200

    adding CORS support

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const routeFactory = require('./routes/factory');
 
 const PORT = 8090;
 
-const server = http.createServer((req, res) => routeFactory(req, res));
+const server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  routeFactory(req, res);
+});
 
 server.listen(PORT);
